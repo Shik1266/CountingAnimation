9e4e7c9d48f45545f81f5d95aba3bb8e9bc1eb9e
hskim <hskim@jasongroup.co.kr>
2024-03-14 10:44:20 +0900

    init

---
+++ example/lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:counting_animation/counting_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+        title: 'Counting Animation Demo',
+        value: 12345,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+  final String title;
+  final int value;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int intvalue = 0;
+  String var1 = '';
+  String var2 = '';
+  String var3 = '';
+  String var4 = '';
+
+  void initData(int value) {
+    intvalue = value;
+    var formatCurrency = NumberFormat.simpleCurrency(
+      locale: "ko_KR",
+      name: '',
+      decimalDigits: 0,
+    );
+    String formatStr = formatCurrency.format(value);
+
+    var1 = value.toString();
+    var2 = '₩${value.toString()}.123';
+    var3 = '₩$formatStr';
+    var4 = '[$formatStr]';
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initData(widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () {
+          initData(intvalue + 1111);
+        },
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            CountingAnimation(
+              value: var1,
+              textStyle: const TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              scrollCount: 20,
+              singleScollDuration: const Duration(milliseconds: 50),
+              lastDuration: const Duration(milliseconds: 1000),
+            ),
+            const Spacer(),
+            CountingAnimation(
+              value: var2,
+              textStyle: const TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+              useChar: true,
+            ),
+            const Spacer(),
+            CountingAnimation(
+              value: var3,
+              textStyle: const TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+              useChar: true,
+            ),
+            const Spacer(),
+            CountingAnimation(
+              value: var4,
+              textStyle: const TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              ),
+              useChar: true,
+              scrollCount: 10,
+              singleScollDuration: const Duration(milliseconds: 200),
+              lastDuration: const Duration(milliseconds: 1000),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/counting_animation.dart
@@ -0,0 +1,160 @@
+library counting_animation;
+
+import 'package:flutter/material.dart';
+
+class CountingAnimation extends StatefulWidget {
+  const CountingAnimation({
+    super.key,
+    required this.value,
+    required this.textStyle,
+    this.scrollCount = 40,
+    this.singleScollDuration = const Duration(milliseconds: 50),
+    this.lastDuration = const Duration(milliseconds: 50),
+    this.bgColor = Colors.white,
+    this.useChar = false,
+  });
+
+  final String value;
+  final TextStyle textStyle;
+  final int scrollCount;
+  final Duration singleScollDuration;
+  final Duration lastDuration;
+  final Color bgColor;
+  final bool useChar;
+
+  @override
+  State<CountingAnimation> createState() => _CountingAnimationState();
+}
+
+class _CountingAnimationState extends State<CountingAnimation>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+  late String strValue = widget.value;
+
+  int tempCnt = 0;
+  bool isLast = false;
+  List<String> fixedString = [];
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.useChar) {
+      for (var char in strValue.characters) {
+        bool a = int.tryParse(char) != null;
+        if (!a) {
+          fixedString.add(char);
+        }
+      }
+    }
+
+    controller = AnimationController(
+      duration: widget.singleScollDuration,
+      vsync: this,
+    );
+    controller.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          if (tempCnt < widget.scrollCount) {
+            tempCnt++;
+            setState(() {});
+            controller.reset();
+            controller.forward();
+          } else if (tempCnt == widget.scrollCount && !isLast) {
+            // tempCnt++;
+            setState(() {
+              isLast = true;
+            });
+            controller.reset();
+            controller.duration = widget.lastDuration;
+            controller.forward();
+          } else {
+            // isLast = false;
+          }
+        }
+      },
+    );
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void initData() {
+    if (widget.useChar) {
+      for (var char in strValue.characters) {
+        bool isInt = int.tryParse(char) != null;
+        if (!isInt) {
+          fixedString.add(char);
+        }
+      }
+    } else {
+      fixedString.clear();
+    }
+    isLast = false;
+    strValue = widget.value;
+    tempCnt = 0;
+    controller.duration = widget.singleScollDuration;
+  }
+
+  @override
+  void didUpdateWidget(covariant CountingAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value) {
+      initData();
+      controller.reset();
+      controller.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(strValue.length, (idx) {
+          if (fixedString.contains(strValue[idx])) {
+            return Text(
+              strValue[idx],
+              style: widget.textStyle,
+            );
+          }
+          return Container(
+            color: widget.bgColor,
+            child: Stack(
+              children: [
+                SlideTransition(
+                  position: controller.drive(
+                    Tween<Offset>(
+                      begin: Offset.zero,
+                      end: const Offset(0, -1), // Out of view above the top.
+                    ),
+                  ),
+                  child: Text(
+                    '${(tempCnt % 10 + (int.parse(strValue[idx]) - 1).abs()) % 10}',
+                    style: widget.textStyle,
+                  ),
+                ),
+                SlideTransition(
+                  position: controller.drive(
+                    Tween<Offset>(
+                      begin:
+                          const Offset(0, 1), // Out of view below the bottom.
+                      end: Offset.zero,
+                    ),
+                  ),
+                  child: Text(
+                    '${isLast ? strValue[idx] : (tempCnt % 10 + int.parse(strValue[idx])) % 10}',
+                    style: widget.textStyle,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
